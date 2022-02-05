d434450897db941dd6dfc7e592f2d81a467abfcd
Deo <klpdeo03@gmail.com>
2022-02-05 23:40:33 +0800

    login-logout for user has been added

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,7 @@ from app import views
 
 urlpatterns = [
     path("", views.render_login, name="render_login"),
-    path("home/", views.render_home, name="render_home")
+    path("login", views.perform_login, name="perform_login"),
+    path("home", views.render_home, name="render_home"),
+    path("logout", views.perform_logout, name="perform_logout")
 ]
--- app/views.py
+++ app/views.py
@@ -1,9 +1,29 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
 
 # Create your views here.
 
 def render_login(request):
     return render(request, "auth_login.html")
 
+def perform_login(request):
+    if request.method != "POST":
+        return HttpResponse("Method not Allowed")
+    else:
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user_obj = authenticate(request, username=username, password=password)
+        if user_obj is not None:
+            login(request, user_obj)
+            return HttpResponseRedirect(reverse("render_home"))
+        else:
+            return HttpResponseRedirect("/")        
+            
 def render_home(request):
     return render(request, "home.html")
+
+def perform_logout(request):
+    logout(request)
+    return HttpResponseRedirect("/")
